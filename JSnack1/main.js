f57aec2b374fc8f703940decdd720da236eb7cff
FrancescoDale <fd060862@gmail.com>
2020-11-02 16:27:34 +0100

    creao l'array con gli oggetti biciclette

---
+++ JSnack1/main.js
@@ -0,0 +1,15 @@
+// array che contiene i vari oggetti bicicletta
+
+var bykeIndex;
+
+bykeIndex = [
+
+    {
+        'model' = 'type0',
+        'wheight' = 6
+    },
+    {
+        'model'
+    }
+
+]
