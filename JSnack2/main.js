41e50c36b0320eed5b231b7be2488650c55fa7c3
FrancescoDale <fd060862@gmail.com>
2020-11-05 19:36:09 +0100

    array di nomi + array dove andranno inseriti i nomi dell'intervallo di posizioni inserito dall'utente

--- JSnack2/main.js
+++ JSnack2/main.js
@@ -1,1 +1,39 @@
 // creao array con vari nomi
+var nameIndex;
+
+// array dove vengono inseriti gli oggetti che fanno parte dell'intervallo scelto dall'utente
+var outputIndex;
+
+nameIndex = [
+
+    {
+        'nome' :'Erika',
+    },
+    {
+        'nome' : 'Alessandra',
+    },
+    {
+        'nome' : 'Giulia',
+    },
+    {
+        'nome' : 'Andrea',
+    },
+    {
+        'nome' : 'Simone',
+    },
+    {
+        'nome' : 'Federico',
+    },
+    {
+        'nome' : 'Giuseppe',
+    },
+    {
+        'nome' : 'Francesca',
+    },
+    {
+        'nome' : 'Mattia',
+    },
+
+];
+
+//chiedo all'utente di inserire due numeri interi compresi tra 0 e numero di oggetti dell'array ( in questo caso è 9 )
